10131d96948691150ee40e03e071957e565d1b36
Sarju-P <charming.sap@gmail.com>
2019-12-14 20:04:43 -0500

    Getting the list of genres

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+const express = require('express');
+const app=express();
+
+const genres = 
+[
+    { id: 1, name: 'Action'},
+    { id: 2, name: 'Horror'},
+    { id: 3, name: 'Romantic' }
+]
+app.get('/', (req,res)=>{
+    res.send(genres);
+});
+
+app.listen(4000,(req,res)=> {
+    console.log('Listening on port 4000...');
+})
+
+
