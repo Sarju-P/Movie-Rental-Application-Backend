435ed3aaa20459316ec113019c2ac1fc6fe34250
Sarju-P <charming.sap@gmail.com>
2019-12-14 21:39:33 -0500

    Returning a genre of movie based on Id

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app=express();
 
 const genres = 
 [
-    { id: 1, name: 'Action'},
-    { id: 2, name: 'Horror'},
+    { id: 1, name: 'Horror'},
+    { id: 2, name: 'Action'},
     { id: 3, name: 'Romantic' }
 ]
 app.get('/', (req,res)=>{
@@ -15,7 +15,14 @@ app.get('/genres', (req,res)=>{
     res.send(genres);
 });
 
+app.get('/genres/:id',(req,res)=>{
+    const genre=genres.find(g=>g.id===parseInt(req.params.id));
+    res.send(genre);
+});
+
+
 const port = process.env.PORT || 3000;
+//To set port : set PORT=4000
 
 app.listen(port,(req,res)=> {
     console.log(`Listening on port ${port}...`);
